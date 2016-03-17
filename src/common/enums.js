d7c825e0b0b9d7d580c372c0c6c1cabc05fd8d0b
nilanno <aa.kachur@gmail.com>
2016-03-17 18:48:47 +0200

    model normalization

--- src/common/enums.js
+++ src/common/enums.js
@@ -1,27 +1,68 @@
 
 function Enums() {
-    var ctx = this;
 
     this.documents = {
         fake: 'fake_documents',
         unregistered: 'unregistered',
         registered: 'registered',
-        invoice: 'ready_for_registration'
+        invoice: 'ready_for_registration',
+        absent: 'no_documents',
+        import: 'ne_rastamogen'
     };
 
     this.type = {
+        other: 'other',
+        skuter: 'skuter',
         classic: 'classic',
         sport: 'sport',
         street: 'street',
         sportTourist: 'sport tourist',
         enduro: 'enduro',
-        chopper: 'chopper'
+        chopper: 'chopper',
+        custom: 'custom',
+        trike: 'trike',
+        quadro: 'quadro'
     };
 
-    this.region = {
-        kharkiv: 'kharkiv'
-        //todo: add more
+    this.states = {
+        ukraine: {
+            any: "Любой",
+            vinnitsa: "Винница",
+            dnepropetrovsk: "Днепропетровск",
+            donetsk: "Донецк",
+            gitomir: "Житомир",
+            zaporozie: "Запорожье",
+            ivanofrankovsk: "Ивано-Франковск",
+            kerch: "Керчь",
+            kiev: "Киев",
+            kirovograd: "Кировоград",
+            kremenchyg: "Кременчуг",
+            krivoirog: "Кривой Рог",
+            crimia: "Крым",
+            lygansk: "Луганск",
+            lyck: "Луцк",
+            lviv: "Львов",
+            melitopol: "Мелитополь",
+            nikolaev: "Николаев",
+            odessa: "Одесса",
+            poltava: "Полтава",
+            rovno: "Ровно",
+            sevastopol: "Севастополь",
+            simferopol: "Симферополь",
+            symi: "Сумы",
+            ternopol: "Тернополь",
+            uzgorod: "Ужгород",
+            feodosia: "Феодосия",
+            kharkiv: "Харьков",
+            herson: "Херсон",
+            hmelnitskii: "Хмельницкий",
+            cherkassi: "Черкассы",
+            chernigov: "Чернигов",
+            chernovtsi: "Черновцы"
+        }
     };
+
+
 
 }
 
--- src/crawler/motosale/modelNormalizer.js
+++ src/crawler/motosale/modelNormalizer.js
@@ -1,7 +1,62 @@
+var enums = require('../../common/enums'),
+    extendFn = require('util')._extend,
+    ukraineStates = enums.states.ukraine,
+    registrationStates = enums.documents;
 
 
-function ModelNormalizer () {
+var statesMap = {
+        "Любой": ukraineStates.any,
+        "Винница": ukraineStates.vinnitsa,
+        "Днепропетровск": ukraineStates.dnepropetrovsk,
+        "Донецк": ukraineStates.donetsk,
+        "Житомир": ukraineStates.gitomir,
+        "Запорожье": ukraineStates.zaporozie,
+        "Ивано-Франковск": ukraineStates.ivanofrankovsk,
+        "Керчь": ukraineStates.kerch,
+        "Киев": ukraineStates.kiev,
+        "Кировоград": ukraineStates.kirovograd,
+        "Кременчуг": ukraineStates.kremenchyg,
+        "Кривой Рог": ukraineStates.krivoirog,
+        "Крым": ukraineStates.crimia,
+        "Луганск": ukraineStates.lygansk,
+        "Луцк": ukraineStates.lyck,
+        "Львов": ukraineStates.lviv,
+        "Мелитополь": ukraineStates.melitopol,
+        "Николаев": ukraineStates.nikolaev,
+        "Одесса": ukraineStates.odessa,
+        "Полтава": ukraineStates.poltava,
+        "Ровно": ukraineStates.rovno,
+        "Севастополь": ukraineStates.sevastopol,
+        "Симферополь": ukraineStates.simferopol,
+        "Сумы": ukraineStates.symi,
+        "Тернополь": ukraineStates.ternopol,
+        "Ужгород": ukraineStates.uzgorod,
+        "Феодосия": ukraineStates.feodosia,
+        "Харьков": ukraineStates.kharkiv,
+        "Херсон": ukraineStates.herson,
+        "Хмельницкий": ukraineStates.hmelnitskii,
+        "Черкассы": ukraineStates.cherkassi,
+        "Чернигов": ukraineStates.chernigov,
+        "Черновцы": ukraineStates.chernovtsi
+    },
+    registrationMap = {
+        'Без документов': registrationStates.absent,
+        'Модель не для дорог общего пользования': registrationStates.absent,
+        'Не растаможен(есь зарубежные доки)': registrationStates.import,
+        '"Свои" документы(деланые)': registrationStates.fake,
+        'Снят с учета': registrationStates.unregistered,
+        'Стоит на укр.учете': registrationStates.registered,
+        'Справка-счет на покупателя': registrationStates.invoice
+    };
 
+
+function ModelNormalizer(object) {
+    var copy = extendFn({}, object);
+
+    copy.region = statesMap[object.region];
+    copy.documents = registrationMap[object.documents];
+
+    return copy;
 }
 
 module.exports = modelNormalizer;
